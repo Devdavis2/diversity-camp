9d52c39fa72ff3fead38fff24355e6e7bf097396
Devdavis2 <dev.davis2@gmail.com>
2020-07-14 14:40:22 -0400

    Built out microsite components and added copy

---
+++ src/Components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Home extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1> Virtual Summer Retreat</h1>
+                <h2> Sub Headline</h2>
+                <br/>
+                <h3>Mission</h3>
+               
+                <p>Our mission is to connect children (virtually) across diverse backgrounds in order to instill empathy <br/> 
+                and understanding and disrupt biases and generalizations by providing exposure to others’ cultures and <br/>
+                experiences and facilitating opportunities for students to examine their perceptions of the world.</p>
+            </div>
+        )
+    }
+}
+
+export default Home;
---
+++ src/Components/Overview.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+class Overview extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>Virtual Summer Retreat Overview</h1>
+                    <p>During the Virtual Summer Retreat, your child will have the opportunity to connect with a diverse group of peers<br/>
+                    and engage in discussions and project-based learning that will deepen their understanding of how experiences shape <br/>
+                    people’s perceptions of the world. These discussions and projects are designed to address the major upper elementary <br/>
+                    Common Core English Language Arts standards. Our goal is for students to be challenged intellectually while working <br/>
+                    together to solve problems, thereby providing authentic opportunities to exchange ideas and collaborate, in an effort <br/>
+                    to dispel societal prejudices. 
+                    <br/>
+                    <br/>
+                    <b>Participants can expect to develop skills such as:</b>
+                        <ul>
+                        <li>making inferences</li>
+                        <li>determining and analyzing themes</li>
+                        <li>evaluating sources</li>
+                        <li>analyzing point of view/perspective</li>
+                        <li>supporting ideas with examples and evidence</li>
+                        <li>posing questions</li>
+                        <li>elaborating on the remarks of others</li>
+                        <li>summarizing the key ideas expressed and drawing conclusions in light of information and knowledge gained from discussion</li>
+                        <li>generating and comparing possible solutions to problems</li>
+                        <li>developing and using models to organize information</li>
+                        </ul>
+
+                    <h2>Virtual Summer Retreat Session Dates/Times:</h2>
+                        <ul>              
+                        <li>Session A: July 27-31 (2pm-4pm)</li>
+                        <li>Session B: August 3-7 (10am-12pm)</li>
+                        <li>Session C: August 3-7 (1pm-3pm)</li>
+                        <li>Session D: August 10-14 (10am-12pm)</li>
+                        <li>Session E: August 10-14 (1pm-3pm)</li>
+                        </ul>
+<br/>
+<b>Cost: Free to participants</b>
+</p>
+            </div>
+        )
+    }
+}
+
+export default Overview;

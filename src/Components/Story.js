e16be93ce7bc65f072696d1c2de228e6abac3255
Devdavis2 <dev.davis2@gmail.com>
2020-07-15 11:22:16 -0400

    update copy

--- src/Components/Story.js
+++ src/Components/Story.js
@@ -5,11 +5,15 @@ class Story extends React.Component {
         return (
             <div className = "container">
                 <h1>Our Story</h1>
-                <p>Recent events have exposed the racial tensions that exist as a result of the longstanding injustices that have been in place for people of color. Many of these injustices have resulted from the biases that are perpetuated when a person has limited opportunities for interaction with people outside of their own race and culture. 
-
-Simultaneously, due to the COVID-19 pandemic and closure of schools we, like most teachers, have had to develop the unique skill set of teaching kids remotely. We saw students thrive in unimaginable ways and realized that this virtual learning model could be used to facilitate connections for students across varied demographics. 
-
-Collage Cooperative holds the belief that systemic change can occur if students create and maintain a deeper understanding of people of other cultures, races, and backgrounds. We are leveraging technology to give students an authentic experience to work with others who, because of geography, they may not have interacted with otherwise. We want to work alongside families to reinforce their values and foster a well-rounded view of the world. Ultimately, we aim to create a safe space for students to learn with and from diverse educators and peers. 
+                <p>Recent events have exposed the racial tensions that exist as a result of the longstanding injustices that have been in place for people of color. 
+                Many of these injustices have resulted from the biases that are perpetuated when a person has limited opportunities for interaction with people outside 
+                of their own race and culture. Simultaneously, due to the COVID-19 pandemic and closure of schools we, like most teachers, have had to develop the unique 
+                skill set of teaching kids remotely. We saw students thrive in unimaginable ways and realized that this virtual learning model could be used to facilitate 
+                connections for students across varied demographics. 
+                
+                <br/>
+                <br/>
+                Collage Cooperative holds the belief that systemic change can occur if students create and maintain a deeper understanding of people of other cultures, races, and backgrounds. We are leveraging technology to give students an authentic experience to work with others who, because of geography, they may not have interacted with otherwise. We want to work alongside families to reinforce their values and foster a well-rounded view of the world. Ultimately, we aim to create a safe space for students to learn with and from diverse educators and peers. 
 </p>
             </div>
         )

01c956e5d0131c0d3d330c90aadf5f27fb390ffd
Devdavis2 <dev.davis2@gmail.com>
2020-07-15 12:08:03 -0400

    updated styling and copy

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ class Home extends React.Component {
         return (
             <div className = "container">
                 <h1> Virtual Summer Retreat</h1>
-                <h2> Sub Headline</h2>
+                <h4> Sub Headline</h4>
                 <p><b>Our mission</b> is to connect children (virtually) across diverse backgrounds in order to instill empathy 
                 and understanding and disrupt biases and generalizations by providing exposure to others’ cultures and
                 experiences and facilitating opportunities for students to examine their perceptions of the world.</p>
